685d6c2bee79bbffe2f491d5c5bc53816313b97d
gyuheeb <gyuhee4529@naver.com>
2023-01-12 14:56:05 +0900

    emillist #2

---
+++ jdbc-practice/src/main/java/emaillist/dao/EmaillistDao.java
@@ -0,0 +1,17 @@
+package emaillist.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import emaillist.vo.EmaillistVo;
+
+public class EmaillistDao {
+
+	
+
+	public List<EmaillistVo> findAll() {
+		List<EmaillistVo> result = new ArrayList<>();
+		return null;
+	}
+
+}
