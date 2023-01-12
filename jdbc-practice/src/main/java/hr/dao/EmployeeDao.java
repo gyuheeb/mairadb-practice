10a82ee5e9deef2de2ce1a4d105054af6d93a394
gyuheeb <gyuhee4529@naver.com>
2023-01-12 14:04:48 +0900

    hr #1

---
+++ jdbc-practice/src/main/java/hr/dao/EmployeeDao.java
@@ -0,0 +1,73 @@
+package hr.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import hr.vo.EmployeeVo;
+
+public class EmployeeDao {
+	public List<EmployeeVo> findByName(String keyword) {
+		List<EmployeeVo> result =new ArrayList<>();
+		
+			ResultSet rs =null;
+			Connection conn =null;
+			PreparedStatement pstmt =null;
+			try {
+				
+				Class.forName("org.mariadb.jdbc.Driver");
+			
+				String url = "jdbc:mariadb://192.168.10.118:3307/employees?charset=utf8";
+				 conn = DriverManager.getConnection(url,"hr","hr");				// 3. Statement 준비
+				 String sql = 
+						 "select emp_no, first_name ,last_name, date_format(hire_date,'%Y-%m-%d')"
+						 +"   from employees"+"  where first_name like ?"+"   or last_name like '%pat%'";
+								 
+				 pstmt =  conn.prepareStatement(sql);
+				 
+				 //4. binding
+				 pstmt.setString(1,"%" + keyword +"%");
+				 pstmt.setString(2,"%" + keyword +"%");
+				
+				rs =pstmt.executeQuery();
+								//5.결과 처리
+				while(rs.next()) {
+					Long no =rs.getLong(1);
+					String firstName=rs.getString(2);
+					String lastName=rs.getString(3);
+					String hire_date=rs.getString(4);
+					
+					EmployeeVo vo = new EmployeeVo();
+					vo.setNo(no);
+					vo.setFirstName(firstName);
+					vo.setLastName(lastName);
+					vo.setHireDate(hire_date);
+					
+					result.add(vo);
+				}
+				
+			} catch (ClassNotFoundException e) {
+				System.out.println("드라이버 로딩 실패: "+e);
+			} catch(SQLException e) {
+				System.out.println("error: "+ e);
+			}finally {
+				try {
+					if(rs != null) {
+						rs.close();
+					}
+					if(pstmt != null) {
+						pstmt.close();
+					}
+					
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		return result;
+	}
+}
